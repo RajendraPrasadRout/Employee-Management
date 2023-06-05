53d4e035c85013e9cba60f0c0a24982627bc9d34
Rajendra <rajendra.rout@ajatus.co.in>
2023-06-05 11:30:47 +0530

    Confirm Password Field

--- Models/UserCredentials.cs
+++ Models/UserCredentials.cs
@@ -11,7 +11,15 @@ namespace MyWebApplicationCRUD.Models
         public string? Username { get; set; }
 
         [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string? ConfirmPassword { get; set; }
 
         [Required]
         public string? UserType { get; set; }
